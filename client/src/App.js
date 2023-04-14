b0cf44aa25d8a40ae17bbe7e6c1b5e00b7e3caf0
simicity <simi40nm@gmail.com>
2023-04-14 12:20:17 -0700

    Add post read function

--- client/src/App.js
+++ client/src/App.js
@@ -8,38 +8,16 @@ import { Link } from 'react-router-dom'
 
 
 const App = () => {
-  
-  const descr = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-
-  const posts = [
-      {'id':'1', 
-      'title': 'Cartwheel in Chelsea 🤸🏽‍♀️',
-      'author':'Harvey Milian', 
-      'description': descr},
-      {'id':'2', 
-      'title': 'Love Lock in Paris 🔒',
-      'author':'Beauford Delaney', 
-      'description':descr},
-      {'id':'3', 
-      'title': 'Wear Pink on Fridays 🎀',
-      'author':'Onika Tonya', 
-      'description':descr},
-      {'id':'4', 
-      'title': 'Adopt a Dog 🐶',
-      'author':'Denise Michelle', 
-      'description':descr},
-  ]
- 
 
   // Sets up routes
   let element = useRoutes([
     {
       path: "/",
-      element:<ReadPosts data={posts}/>
+      element:<ReadPosts />
     },
     {
       path:"/edit/:id",
-      element: <EditPost data={posts} />
+      element: <EditPost />
     },
     {
       path:"/new",
--- client/src/pages/ReadPosts.js
+++ client/src/pages/ReadPosts.js
@@ -1,24 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
 
-const ReadPosts = (props) => {
+const ReadPosts = () => {
 
     const [posts, setPosts] = useState([]);
+    const fetchPosts = async () => {
+        const response = await fetch("http://localhost:8000/posts", {
+            method: "GET",
+            mode: "cors",
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+        setPosts(data.rows); 
+    }
 
     useEffect(() => {
-        setPosts(props.data);
-    }, [props]);
+        fetchPosts();
+    });
     
     return (
         <div className="ReadPosts">
             {
                 posts && posts.length > 0 ?
-                posts.map((post,index) => 
-                   <Card id={post.id} title={post.title} author={post.author} description={post.description}/>
+                posts.map((post, index) => 
+                   <Card key={post.rowid} id={post.rowid} title={post.title} author={post.author} description={post.description}/>
                 ) : <h2>{'No Challenges Yet 😞'}</h2>
             }
         </div>  
-    )
+    );
 }
 
 export default ReadPosts;
