354d7de06844289c81d3b109be6137b919f0528b
simicity <simi40nm@gmail.com>
2023-04-15 13:17:35 -0700

    Add post creation function

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,19 +2,41 @@ import React from 'react';
 import './CreatePost.css'
 
 const CreatePost = () => {
+    const createPost = async (event) => {
+        event.preventDefault();
+
+        // Set redirect
+        window.location = "/";
+
+        const response = await fetch("http://localhost:8000/post", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": event.target.title.value,
+                "author": event.target.author.value,
+                "description": event.target.description.value,
+            }),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+    }
 
     return (
         <div>
-            <form>
-                <label for="title">Title</label> <br />
+            <form onSubmit={createPost}>
+                <label htmlFor="title">Title</label> <br />
                 <input type="text" id="title" name="title" /><br />
                 <br/>
 
-                <label for="author">Author</label><br />
+                <label htmlFor="author">Author</label><br />
                 <input type="text" id="author" name="author" /><br />
                 <br/>
 
-                <label for="description">Description</label><br />
+                <label htmlFor="description">Description</label><br />
                 <textarea rows="5" cols="50" id="description">
                 </textarea>
                 <br/>
